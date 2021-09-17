5d144ae943fb390d01d615e59840da8aa2a54436
SailingV <90022477+SailingV@users.noreply.github.com>
2021-09-17 17:28:08 +0900

    09/17 Fill_1

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,91 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@rowb
+M=0	//rowb=0
+@colb
+M=0	//colb=0
+@16
+D=A
+@high
+M=D	//high = 16
+@512
+D=A
+@highcal
+M=D	//highcal = 512
+@SCREEN
+D=A
+@sc
+M=D	//sc = SCREEN
+@addrrm
+M=0	//addrrm = 0
+@i
+M=0	//i=0
+@n
+M=0	//n=0
+//초기화
+
+
+
+@R0
+D=M
+@rowb
+M=D	//rowb = R0
+@R1
+D=M
+@colb
+M=D	//colb = R1
+@sc
+D=M
+@addrrm
+M=D
+@colb
+D=M
+@addrrm
+M=M+D	//addrrm = screen+colb
+
+(LOOPM)
+	@highcal
+	D=M
+	@temp
+	M=M+D
+	@i
+	M=M+1
+	D=M
+	@n
+	M=D
+	@rowb
+	D=M	
+	@n
+	M=M-D
+	D=M
+	@LOOPM
+	D;JLT
+	//LOOPM:
+	//	temp = temp + highcal
+	//	i++
+	//	if i<rowb goto LOOPM
+@temp
+D=M
+@addrrm
+M=M+D	//addrrm = addrrm + temp
+@i
+M=0	//i=0
+@temp
+M=0	//temp=0
+//addrrm = screen+colb+rowb*highcal 검은색 블록 초기 위치설정
+@addrrm
+D=M
+@addr
+M=D
+@addrmov
+M=D	//addr 백업값
+
+(END)
+@END
+0;JMP	//종료
+
+
+	
+
